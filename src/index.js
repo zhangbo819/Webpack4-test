75af4b18332e774c6461cc69ed419c60c6967486
zzb <https://github.com/zhangbo819/-.git>
2019-03-16 22:47:53 +0800

    tree shaking

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import _ from 'lodash';
 import './style.css';
+import { cube } from './math.js';
+
 
 function component() {
-    var element = document.createElement('div');
+    var element = document.createElement('pre');
 
-     // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+    element.innerHTML = [
+        'Hello webpack!',
+        '5 cubed is equal to ' + cube(5)
+    ].join('\n\n');
 
     return element;
 }
