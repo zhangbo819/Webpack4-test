6b1e6a4ba1343c0fd12c2cc0e268633d39d5bb28
zzb <https://github.com/zhangbo819/-.git>
2019-03-16 23:33:55 +0800

    splitChunks

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,40 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    index: './src/index.js',
-    another: './src/another-module.js'
-  },
-  plugins: [
-    new HTMLWebpackPlugin({
-      title: 'Code Splitting'
-    })
-  ],
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    entry: {
+        index: './src/index.js',
+        another: './src/another-module.js'
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            title: 'Code Splitting'
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    optimization: {
+        splitChunks: {
+          chunks: 'async',
+          minSize: 30000,
+          maxSize: 0,
+          minChunks: 1,
+          maxAsyncRequests: 5,
+          maxInitialRequests: 3,
+          automaticNameDelimiter: '~',
+          name: true,
+          cacheGroups: {
+            vendors: {
+              test: /[\\/]node_modules[\\/]/,
+              priority: -10
+            },
+            default: {
+              minChunks: 2,
+              priority: -20,
+              reuseExistingChunk: true
+            }
+          }
+        }
+      }
 };
